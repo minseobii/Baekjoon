67875c81e61063a0c92f7cad860994e4a938bc6f
minseobii <yqmt8708@naver.com>
2024-06-02 17:24:08 +0900

    solving

---
+++ problem/1260.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int N,M,V;
+
+int arr[1001][1001];
+int visited_dfs[1001];
+int visited_bfs[1001];
+
+int DFS(int a) {
+    visited_bfs[a] = 1;
+    for (int i=1;i<)
+}
+
+int BFS() {
+
+}
+
+int main() {
+    cin >> N >> M >> V;
+    DFS(V);
+    BFS(V);
+    cout << ans << endl;
+    return 0;
+}
