e1c266ec4e924d1a60651c7b5e8265a28e4736de
minseobii <yqmt8708@naver.com>
2024-06-02 17:09:00 +0900

    search

--- problem/2667.cpp
+++ problem/2667.cpp
@@ -1,8 +1,71 @@
 #include <iostream>
 using namespace std;
+
+int N;
+char arr[26][26];
+
+int ans=0;
+int ans_arr[1000];
+
+int search(int i, int j) {
+    if (arr[i][j] == '1') {
+            ans_arr[ans]++;
+            arr[i][j] = 0;
+            if (i+1 < N) {
+                search(i+1,j);
+            }
+            if (j+1 < N) {
+                search(i,j+1);
+            }
+            if (i > 0) {
+                search(i-1,j);
+            }
+            if (j > 0) {
+                search(i,j-1);
+            }
+            return 1;
+    }
+    return 0;
+}
+
 int main() {
-    auto a=0, b=0;
-    cin >> a >> b;
-    cout << a+b << endl;
+    cin >> N;
+
+    for (int i=0;i<N;i++) { //input
+       for (int j=0;j<N;j++) {
+        char house;
+        cin >> house;
+        arr[i][j] = house;
+       }
+    } 
+
+    for (int i=0;i<N;i++) { //search
+       for (int j=0;j<N;j++) {
+        if (arr[i][j] == '1') {
+            if (search(i,j)) {
+                ans++;
+            }
+        }
+       }
+    } 
+
+    int min = ans_arr[0];
+
+    for (int i=0;i<ans-1;i++) { //sort
+        int min = i;
+        for (int j=i+1;j<ans;j++) {
+            if (ans_arr[i] >= ans_arr[j]) {
+                int temp = ans_arr[i];
+                ans_arr[i] = ans_arr[j];
+                ans_arr[j] = temp;
+            }
+        }
+    } 
+
+    cout << ans << endl;
+
+    for (int i=0;i<ans;i++) {
+        cout << ans_arr[i] << endl;
+    }    
     return 0;
 }
--- problem/_template.cpp
+++ problem/_template.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+int N;
+int ans;
 int main() {
-    auto a=0, b=0;
-    cin >> a >> b;
-    cout << a+b << endl;
+    cin >> N;
+    cout << ans << endl;
     return 0;
 }
