441f3afeaebc1cefc26e390072bab53ca72f117f
minseobii <yqmt8708@naver.com>
2024-06-04 14:13:05 +0900

    graph  구현 필요

--- problem/1260.cpp
+++ problem/1260.cpp
@@ -3,22 +3,55 @@ using namespace std;
 int N,M,V;
 
 int arr[1001][1001];
+
 int visited_dfs[1001];
 int visited_bfs[1001];
 
+int queue[1001]; 
+
 int DFS(int a) {
-    visited_bfs[a] = 1;
-    for (int i=1;i<)
+    visited_dfs[a] = 1;
+    cout << a << ' ';
+    for (int i=1;i<=N;i++) {
+        if (arr[a][i] && !visited_dfs[i]) {
+            DFS(i);
+        }
+    }
+    return 0;
 }
 
-int BFS() {
-
+int BFS(int start, int end) {
+    int end_new = end;
+    for (int i=start;i<end;i++) {
+        int q = queue[i];
+        for (int j=1;j<=N;j++) {
+            if (arr[q][j] && !visited_bfs[j]) {
+                visited_bfs[j] = 1;
+                cout << j << ' ';
+                queue[end_new++] = j;
+            }
+        }
+    }
+    if (end < end_new) BFS(end, end_new);
+    return 0;
 }
 
 int main() {
     cin >> N >> M >> V;
+
+    for (int i=0; i<M; i++) { //간선 생성
+        int a,b;
+        cin >> a >> b;
+        arr[a][b] = 1;
+        arr[b][a] = 1;
+    }
+
     DFS(V);
-    BFS(V);
-    cout << ans << endl;
+    
+    cout << endl << V << ' ';
+    visited_bfs[V] = 1;
+    queue[0] = V;
+    BFS(0,1);
+
     return 0;
 }
