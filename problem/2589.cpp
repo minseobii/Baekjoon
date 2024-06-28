f9b809c805af1dd6431e63278c5a0f533051116d
minseobii <yqmt8708@naver.com>
2024-06-29 00:00:15 +0900

    solving

--- problem/2589.cpp
+++ problem/2589.cpp
@@ -3,13 +3,91 @@
 using namespace std;
 int N,M;
 char arr[51][51];
+int group[51][51];
+int group_num=1;
 int dy[4] = {1,0,-1,0};
 int dx[4] = {0,1,0,-1};
-int ans;
+
+int ans=0;
 
 struct PAIR{
     int i,j,depth;
 };
+
+int checkmindepth(int i,int j,int x,int y){
+    int visit[51][51];
+    int min=0;
+    queue<PAIR> q;
+                
+    PAIR pair = {i,j,0};
+    q.push(pair);
+
+    while (!q.empty()) {
+
+        int a = q.front().i;
+        int b = q.front().j;
+        int depth = q.front().depth;
+        q.pop();
+
+        if (a==x && b==y) {
+            if (min == 0) {
+                min = depth;
+            }
+            else {
+                if (min > depth) {
+                    min = depth;
+                }
+            }
+            continue;
+        }
+
+        for (int k=0;k<4;k++) {
+                        
+            int new_a = a + dy[k];
+            int new_b = b + dx[k];
+
+            if (new_a < 0 || new_b < 0 || new_a+1 > N || new_b+1 > M || visit[new_a][new_b] == 1 || arr[new_a][new_b] == 'W') {
+                continue;
+            }
+            PAIR new_pair = {new_a,new_b,depth+1};
+            q.push(new_pair);
+            visit[new_a][new_b] = 1;
+        }
+    }
+    return min;
+}
+
+void putingroup(int i, int j) {
+    int visit[51][51];
+    int group_idx=0;
+    queue<PAIR> q;
+                
+    PAIR pair = {i,j,0};
+    q.push(pair);
+
+    while (!q.empty()) {
+
+        int a = q.front().i;
+        int b = q.front().j;
+        q.pop();
+
+        group[a][b] = group_num;
+        
+
+        for (int k=0;k<4;k++) {
+                        
+            int new_a = a + dy[k];
+            int new_b = b + dx[k];
+
+            if (new_a < 0 || new_b < 0 || new_a+1 > N || new_b+1 > M || visit[new_a][new_b] == 1 || arr[new_a][new_b] == 'W') {
+                continue;
+            }
+            PAIR new_pair = {new_a,new_b,0};
+            q.push(new_pair);
+            visit[new_a][new_b] = 1;
+        }
+    }
+}
 
 int main() {
     cin >> N >> M;
@@ -25,63 +103,35 @@ int main() {
     for (int i=0;i<N;i++) {
         for (int j=0;j<M;j++) {
             if (arr[i][j] == 'L') {
-                int arr_dis[51][51];
-                arr[i][j] = -1;
-                int visit[51][51];
-                queue<PAIR> q;
-                
-                PAIR pair = {i,j,0};
-                q.push(pair);
+               if (group[i][j] == 0) {
+                putingroup(i,j);
+                group_num++;
+               }          
+            }
+        }
+    }
 
-                while (!q.empty()) {
-
-                    int a = q.front().i;
-                    int b = q.front().j;
-                    int depth = q.front().depth;
-                    q.pop();
-
-                    for (int k=0;k<4;k++) {
-                        
-                        int new_a = a + dy[k];
-                        int new_b = b + dx[k];
-
-                        int dis = arr_dis[new_a][new_b];
-                        
-                        if (dis != -1) {
-                            if (dis == 0) {
-                                dis = depth;
-                            }
-                            else {
-                                if (dis > depth) {
-                                    dis = depth;
+    for(int i=1;i<group_num;i++) {
+        for(int a=0;a<N;a++) {
+            for(int b=0;b<M;b++) {
+                if (group[a][b] == i) {
+                    for (int x=a;x<N;x++) {
+                        for (int y=b+1;b<M;b++) {
+                            if (group[x][y] == i) {
+                                int depth = checkmindepth(a,b,x,y);
+                                if (ans > depth) {
+                                    ans = depth;
                                 }
                             }
                         }
-
-                        arr[new_a][new_b] = dis;
-
-                        if (new_a < 0 || new_b < 0 || new_a+1 > N || new_b+1 > M || visit[new_a][new_b] == 1 || arr[new_a][new_b] == 'W') {
-                             continue;
-                        }
-                        PAIR new_pair = {new_a,new_b,depth+1};
-                        q.push(new_pair);
-                        visit[new_a][new_b] = 1;
                     }
-                }
-                int max=0;
-                for (int dis_i=0;dis_i<51;dis_i++) {
-                    for (int dis_j=0;dis_j<51;dis_j++) {
-                        if (max < arr_dis[dis_i][dis_j]) {
-                            max = arr_dis[dis_i][dis_j];
-                        }
-                    }
-                }
-                if (ans < max) {
-                    ans = max;
                 }
             }
         }
     }
+
+    
+    
 
     cout << ans << endl;
     return 0;
