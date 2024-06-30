ba0c42e2960214d0d6275a04c84afc3595ed2504
minseobii <yqmt8708@naver.com>
2024-06-30 18:45:24 +0900

    template

---
+++ template/permutation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[9];
+int subarr[9] = {0,0,1,1,1,1,1,1,1}; 
+int main() {
+    do { 
+        int temparr[7] = {0,};
+    } while(next_permutation(subarr,subarr+9));
+    
+    return 0;
+}
