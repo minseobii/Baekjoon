c4d8cae5a0f200a887983eaf505e4bbb2c20409b
minseobii <yqmt8708@naver.com>
2024-07-28 16:16:21 +0900

    DFS - DFS 내에서의 visted으로 중복 방지

---
+++ solving/15663.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int N,M;
+int arr[10] = {0,};
+int ansarr[10] = {0,};
+int visit[10] = {0,};
+
+void DFS(int a) {
+    if (a == M) {
+        for (int i=0;i<M;i++) {
+            cout << ansarr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    else {
+        int num[10001] = {0,};
+        for (int i=0;i<N;i++) {
+            if (!visit[i] && !num[arr[i]]) {
+                num[arr[i]] = 1;
+                visit[i] = 1;
+                ansarr[a] = arr[i];
+                DFS(a+1);
+                visit[i] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> M;
+    for (int i=0;i<N;i++) {
+        cin >> arr[i];
+    }
+    sort(arr,arr+N);
+    DFS(0);
+    return 0;
+}
