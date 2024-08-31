7d2c8a51a750bef84c659179a4c78aaf6b5322d6
minseobii <yqmt8708@naver.com>
2024-08-31 23:10:33 +0900

    template

---
+++ solving/1562.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int N,ans;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    cout << ans << endl;
+    return 0;
+}
