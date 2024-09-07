b1a0dcc0c43ea3e2e3829a9c4f0d086beedc4b10
minseobii <yqmt8708@naver.com>
2024-09-07 23:51:53 +0900

    solving

---
+++ solving/개미굴.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int N,ans;
+
+int arr[15]
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    while(N--) {
+        int K;
+        cin >> K;
+        for (int i=0;i<K;i++) {
+
+        }
+    }
+    cout << ans << endl; 
+    return 0;
+}
