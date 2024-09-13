0f0ddb9bb6bb12982235417bd1c119a705634da7
minseobii <yqmt8708@naver.com>
2024-09-13 22:47:09 +0900

    template

--- solving/temp.cpp
+++ solving/temp.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
+int #include <iostream>
+using namespace std;
 int N,ans;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    cout << ans << endl;
+    return 0;
+}
+N,ans;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
