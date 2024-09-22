7dafdbe52a131795aef9b97d0f6c7bcbae877afd
minseobii <yqmt8708@naver.com>
2024-09-22 17:06:40 +0900

    solving

---
+++ solving/오아시스 재결합.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <cmath>
+using namespace std;
+int N;
+long long ans;
+int arr[500000];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    for (int i=0;i<N;i++) {
+        cin >> arr[i];
+    }
+
+    
+
+    cout << ans << '\n';
+    return 0;
+}
