5638482fddbb1a572abeb7a1760d3c5f5224b834
minseobii <yqmt8708@naver.com>
2024-10-11 17:12:57 +0900

    solving

---
+++ solving/프렉탈 평면.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+#include <cmath>
+using namespace std;
+int s, N, K, R1, R2, C1, C2, ans[50][50];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> s >> N >> K >> R1 >> R2 >> C1 >> C2;
+
+    
+
+    cout << ans << '\n';
+    return 0;
+}
